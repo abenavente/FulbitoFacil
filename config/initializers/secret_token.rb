8c604b240ff95a905891c10cacdc4a8a718a1708
Alexis.Benavente <lxsbenavente@gmail.com>
2012-01-27 23:41:35 -0500

    Primera Carga

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FulbitoFacil::Application.config.secret_token = 'e695278762d671a4f6da1ad026deacb8eb421af1a360aa7f3fafb3166e437be492fb4317f2e94f5df80f971d3bb0a1ef46604f312615a51989f554b1c1dbe561'
